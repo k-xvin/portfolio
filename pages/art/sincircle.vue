80f574a0cd2d94e79fcf6f3cb58e7ad118520a5e
Kevin Xu <bobthehoboofhobos@gmail.com>
2021-07-12 00:02:12 -0700

    added sincircle

---
+++ pages/art/sincircle.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <v-card height="800px" class="ma-10">
+            <sin-circle></sin-circle>
+        </v-card>
+    </div>
+</template>
+
+<script>
+import SinCircle from "../../components/sketches/SinCircle.vue";
+export default {
+    components: { SinCircle },
+    methods: {},
+};
+</script>
