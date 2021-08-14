c36ddb8589c5f7d20af987f3fab16df8c9f49045
Kevin Xu <bobthehoboofhobos@gmail.com>
2021-08-14 13:47:22 -0700

    added template

---
+++ pages/art/templatesketch.vue
@@ -0,0 +1,11 @@
+<template>
+    <template-sketch></template-sketch>
+</template>
+
+<script>
+import TemplateSketch from "../../components/sketches/TemplateSketch.vue";
+export default {
+    components: { TemplateSketch },
+    methods: {},
+};
+</script>
