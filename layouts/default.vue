f8e16b88326e483ce58b845c88454e38af198cd3
Kevin Xu <bobthehoboofhobos@gmail.com>
2021-12-31 21:43:45 -0800

    error page added

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="main-container">
+        <Nuxt />
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+    .main-container {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
---
+++ layouts/error.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="error-page scanlines">
+        <a v-if="error.statusCode === 404" class="errortext" href="/">
+            [ 404 ]
+        </a>
+        <a v-else class="errortext" href="/"> [ ERROR ] </a>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["error"],
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/css/scanlines.scss";
+
+.error-page {
+    background-image: url("~assets/error.jpg");
+    background-size: cover;
+    background-position: center;
+
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.errortext {
+    font-size: 3rem;
+    text-decoration: none;
+    color: white;
+    background-color: blue;
+    padding: 1rem;
+}
+</style>
